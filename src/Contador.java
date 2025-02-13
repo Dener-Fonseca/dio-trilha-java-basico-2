9bc3bc2993d906abb0de0fbb0d3b0e6175c8dafe
Dener Fonseca <156597735+Dener-Fonseca@users.noreply.github.com>
2025-02-13 17:23:32 +0000

    Assistant checkpoint: Corrigir estrutura da classe e remover variável não usada
    Assistant generated file changes:
    - src/Contador.java: Corrigir estrutura da classe
    
    ---
    
    User prompt:
    
    Qual erro neste código?

--- src/Contador.java
+++ src/Contador.java
@@ -1,66 +1,35 @@
 import java.util.Scanner;
 
 public class Contador {
-  
   public static void main(String[] args) {
-
     Scanner sc = new Scanner(System.in);
-
     System.out.println("Digite o primeiro número: ");
-
     int num1 = sc.nextInt();
-
     System.out.println("Digite o segundo número: ");
-
     int num2 = sc.nextInt();
 
-
     try {
-
       contar(num1, num2);
-
-      }
-
-    catch (ParametrosInvalidosException e) {
-       System.out.println(e.getMessage());
-
-      }
-
-    finally {
-
+    } catch (ParametrosInvalidosException e) {
+      System.out.println(e.getMessage());
+    } finally {
       sc.close();
-     
-    }
-
-    static void contar(int num1, int num2) throws ParametrosInvalidosException {
-
-    if (num1 > num2){
-      throw new ParametrosInvalidosException("O primeiro número deve ser menor que o segundo número!");
-
-      }
-
-      int contagem = num1 - num2;
-
-      for (int i = num1; i <= num2; i++) {
-      
-    System.out.println("Imprimindo o número " + i);
-
-        }
-      }
-
-    public class ParametrosInvalidosException extends Exception {
-
-      public ParametrosInvalidosException(String message) {
-        super(message);
-      }
-
     }
   }
 
+  static void contar(int num1, int num2) throws ParametrosInvalidosException {
+    if (num1 > num2) {
+      throw new ParametrosInvalidosException("O primeiro número deve ser menor que o segundo número!");
+    }
+
+    for (int i = num1; i <= num2; i++) {
+      System.out.println("Imprimindo o número " + i);
+    }
+  }
 }
 
-
-
-      
-
-      
+class ParametrosInvalidosException extends Exception {
+  public ParametrosInvalidosException(String message) {
+    super(message);
+  }
+}
