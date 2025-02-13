cb39ef113a8274875bcdbd73d7f57e29cdf39f94
Dener Fonseca <156597735+Dener-Fonseca@users.noreply.github.com>
2025-02-13 18:51:20 +0000

    Assistant checkpoint: Fixed method placement in Contador.java
    Assistant generated file changes:
    - src/Contador.java: Move contar method outside main
    
    ---
    
    User prompt:
    
    Running the code in my project results in the following:
    
    ```
    src/Contador.java:30: error: illegal start of expression
        void contar(int num1, int num2) throws ParametrosInvalidosException {
        ^
    src/Contador.java:30: error: ';' expected
        void contar(int num1, int num2) throws ParametrosInvalidosException {
                   ^
    src/Contador.java:30: error: <identifier> expected
        void contar(int num1, int num2) throws ParametrosInvalidosException {
                             ^
    src/Contador.java:30: error: ';' expected
        void contar(int num1, int num2) throws ParametrosInvalidosException {
                                      ^
    src/Contador.java:30: error: not a statement
        void contar(int num1, int num2) throws ParametrosInvalidosException {
                                               ^
    src/Contador.java:30: error: ';' expected
        void contar(int num1, int num2) throws ParametrosInvalidosException {
                                                                           ^
    6 errors
    
    ```
    
    If applicable, propose a fix immediately.

--- src/Contador.java
+++ src/Contador.java
@@ -2,50 +2,35 @@ import java.util.Scanner;
 
 public class Contador {
   public static void main(String[] args) {
-    
     Scanner sc = new Scanner(System.in);
-    
-    
+
     System.out.println("Digite o primeiro número: ");
     int num1 = sc.nextInt();
-    
-    
+
     System.out.println("Digite o segundo número: ");
     int num2 = sc.nextInt();
 
-    
     try {
       contar(num1, num2);
-    } 
-    
-    catch (ParametrosInvalidosException e) {
+    } catch (ParametrosInvalidosException e) {
       System.out.println(e.getMessage());
-    } 
-    
-    finally {
+    } finally {
       sc.close();
     }
+  }
 
-    
-    void contar(int num1, int num2) throws ParametrosInvalidosException {
-      
-      if (num1 > num2) {
-        
-        throw new ParametrosInvalidosException("O primeiro número deve ser menor que o segundo número!");
-      } 
-      
-      else {
-        
-        for (int i = num1; i <= num2; i++) {
-          System.out.println("Imprimindo o número " + i);
-        }
+  static void contar(int num1, int num2) throws ParametrosInvalidosException {
+    if (num1 > num2) {
+      throw new ParametrosInvalidosException("O primeiro número deve ser menor que o segundo número!");
+    } else {
+      for (int i = num1; i <= num2; i++) {
+        System.out.println("Imprimindo o número " + i);
       }
     }
   }
 }
 
 class ParametrosInvalidosException extends Exception {
-  
   public ParametrosInvalidosException(String message) {
     super(message);
   }
