c1a8d997c0a2a523ebdd4f1d411542e5040bdbb9
Dener Fonseca <156597735+Dener-Fonseca@users.noreply.github.com>
2025-02-13 19:43:59 +0000

    Atualizando readme

--- src/Contador.java
+++ src/Contador.java
@@ -2,27 +2,40 @@ import java.util.Scanner;
 
 public class Contador {
   public static void main(String[] args) {
+    
     Scanner sc = new Scanner(System.in);
-
+    
     System.out.println("Digite o primeiro número: ");
     int num1 = sc.nextInt();
-
+    
     System.out.println("Digite o segundo número: ");
     int num2 = sc.nextInt();
 
+    tratarContagem(num1, num2);
+    
+    sc.close();
+  }
+
+  static void tratarContagem(int num1, int num2) {
     try {
       contar(num1, num2);
-    } catch (ParametrosInvalidosException e) {
+    } 
+    
+    catch (ParametrosInvalidosException e) {
       System.out.println(e.getMessage());
-    } finally {
-      sc.close();
+    } 
+    
+    finally {
+      System.out.println("Contagem realizada.");
     }
   }
 
   static void contar(int num1, int num2) throws ParametrosInvalidosException {
     if (num1 > num2) {
       throw new ParametrosInvalidosException("O primeiro número deve ser menor que o segundo número!");
-    } else {
+    } 
+    
+    else {
       for (int i = num1; i <= num2; i++) {
         System.out.println("Imprimindo o número " + i);
       }
@@ -31,6 +44,7 @@ public class Contador {
 }
 
 class ParametrosInvalidosException extends Exception {
+  
   public ParametrosInvalidosException(String message) {
     super(message);
   }
